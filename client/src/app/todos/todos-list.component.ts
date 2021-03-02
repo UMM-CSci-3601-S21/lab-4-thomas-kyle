666a9f0988dead986e78b2eb679a7f04c33d85c2
Thomas <dahlg136@morris.umn.edu>
2021-03-02 13:23:11 -0600

    Add server test for filtering by status
    This test makes sure that when implemented the filter by status method will work correctly. Also fixes LGMT alert for unused variable by deleting the variable. Co-authored by Kyle Day day00090@morris.umn.edu

--- client/src/app/todos/todos-list.component.ts
+++ client/src/app/todos/todos-list.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { Todos } from './todos';
 import { TodosService } from './todos.service';
 import { Subscription } from 'rxjs';
